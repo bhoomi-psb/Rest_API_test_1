5bec12750b775a2bf57ea423e6108bde21a92708
bhoomi-psb <desaibhoomikap2003@gmail.com>
2025-03-11 11:16:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify
+
+todo = Flask(__name__)  # Fixing the Flask app name
+
+# Defining the students list outside of functions
+students = [
+    {
+        'id': 1,
+        'student_name': 'std1',
+        'age': 21,
+        'email': 'std1@gmail.com'
+    },
+    {
+        'id': 2,
+        'student_name': 'std2',
+        'age': 21,
+        'email': 'std2@gmail.com'
+    },
+    {
+        'id': 3,
+        'student_name': 'std3',
+        'age': 22,
+        'email': 'std3@gmail.com'
+    }
+]
+
+@todo.route('/students-list')
+def students_list():
+    return jsonify(students)
+
+@todo.route('/students/get/<int:id>')
+def get_student(id):
+    # Fixing the student search logic
+    student = next((s for s in students if s['id'] == id), None)
+    if student:
+        return jsonify(student)
+    else:
+        return jsonify({"error": "Student not found"}), 404
+
+if __name__ == '__main__':
+    todo.run(
+        host='127.0.0.1',
+        port=5010,
+        debug=True
+    )
